6bc67500ff60a9a51aa41228228f653a1ea20467
avdhantyagi <avdhantyagi1011@gmail.com>
2024-08-25 16:30:54 +0530

    Swolenormous checkpoint

---
+++ src/components/Generator.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import SectionWrapper from './SectionWrapper'
+
+import {WORKOUTS} from "../utils/swoldier"
+
+function Header(props){
+    const {index,title,description} = props
+    return(
+        <div className='flex flex-col gap-4'>
+            <div className='flex items-center justify-center gap-2'>
+                <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+                <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+            </div>
+            <p className='text-sm sm:text-base mx-auto'>{description}</p>
+        </div>
+    )
+}
+
+export default function Generator() {
+  return (
+    <SectionWrapper header={"generate your workout"} title={['It\'s','Huge','o\'clock']}>
+        <Header index={'01'} title={'Pick your Poison'} description={'Select the workout'}/>
+        <div className='grid grid-cols-2 sm:grid-cols-4 gap-5'>
+        {Object.keys(WORKOUTS).map((type, typeIndex) =>{
+            return (
+                <button className='bg-slate-950 border-2 border-blue-400 py-4 rounded-lg'key={typeIndex}>
+                    <p>{type}</p>
+                </button>
+            )
+        })}
+        </div>
+    </SectionWrapper>
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 justify-center items-center text-center max-w-[900px] w-full mx-auto p-4'>
+        <div className='flex flex-col gap-2'>
+            <p> IT'S TIME TO GET</p>
+            <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'> Swole<span className = 'text-blue-400'>normous</span></h1>
+        </div>
+        <p className='text-sm md:text-base font-light'> I acknowlege the risk of me becoming an <span className = 'text-blue-400 font-medium'> extremely jacked ass</span> eu nisi esse officia sint dolor nostrud ad cupidatat nulla magna reprehenderit dolor id Lorem ut labore sunt non dolor velit anim exercitation dolor nulla deserunt ut cillum non non occaecat cupidatat ea ut tempor amet non nulla esse nisi aliquip</p>
+        <p><button className='px-8 py-4 rounded-md border-2 border-blue-400 border-solid bg-slate-950 blueShadow duration-200'>Accept and Begin</button></p>
+    </div>
+  )
+}
